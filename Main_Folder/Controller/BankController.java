e4fc0a6f735b50d2f5a3197f86cad94d80fc7402
kesavan <kesavan200418@gamil.com>
2025-08-20 14:39:26 +0530

    Model , view , Controller Jobs are done

---
+++ Main_Folder/Controller/BankController.java
@@ -0,0 +1,112 @@
+package Controller;
+
+import Service.BankServices;    
+import View.CustomerView;
+import java.util.Scanner;
+
+/**
+ * BankController class to manage bank operations.
+ */
+
+public class BankController {
+
+    BankServices bankServices;
+    CustomerView customerView;
+    Scanner sc = new Scanner(System.in);
+
+    public BankController() {
+        this.bankServices = new BankServices();
+        this.customerView = new CustomerView();
+    }
+
+    public void start() {
+        customerView.displayWelcomeMessage();  
+        
+        while (true) {
+            customerView.displayMainMenu();
+            int choice = sc.nextInt();
+
+            swtich(choice) {
+
+                case 1:
+                createAccount();
+                break;
+
+                case 2:
+                login();   
+                break;  
+                
+                case 3:
+                System.out.println("Thank you for using our services!");  
+                return;
+                
+               default:
+               System.out.println("Invalid choice!");
+           }
+     }
+    }
+
+    public createAccount() {
+       System.out.println("Enter your Name");
+       sc.nextLine();
+       String accountHolderName = sc.nextLine();
+       System.out.println("Account created successfully! with initial balance of 10000.");
+       System.out.println("Please Remember your customer_ID and password that given below for future reference and Login.");
+       System.out.println(bankServices.createAccount(accountHolderName));
+
+    }                  
+
+    public void servicesAvailable(String customerId) {
+        customerView.displayServicesMenu();
+        while (true) {
+            int serviceChoice = sc.nextInt();
+
+            switch(serviceChoice) {
+                case 1:
+                    bankServices.deposit(customerId);
+                    break;
+                case 2:
+                    bankServices.ATMwithdraw(customerId);
+                    break;
+                case 3:
+                    bankServices.transfer(customerId);
+                    break;
+                case 4:
+                    bankServices.viewBalance(customerId);
+                    break;  
+                case 5:
+                    bankServices.viewTransactionHistory(customerId);
+                    break;
+                case 6:
+                    bankServices.managePassword(customerId);
+                case 7:
+                    System.out.println("Logging out...");
+                    return; // Exit the services menu
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+                    continue;
+            }
+    }
+
+}
+    
+
+    public void login(){
+
+        System.out.println("Enter your Customer ID:");
+        sc.nextLine();
+        String customerId = sc.nextLine();              
+        System.out.println("Enter your Password:");
+        String password = sc.nextLine();    
+      
+        if(bankServices.authenticateUser(customerId, password)) {
+            System.out.println("Login successful!");
+            servicesAvailable(customerId);
+        }
+        else {
+            System.out.println("Login failed! Due to Invalid Credentials.");
+        }
+
+    }
+    
+}
---
+++ Main_Folder/View/CustomerView.java
@@ -0,0 +1,22 @@
+package View;
+
+public class CustomerView {
+    public void displayServicesMenu() {
+        System.out.println("1. Deposit Money");
+        System.out.println("2. Withdraw  Money from ATM");
+        System.out.println("3. Transfer Money");
+        System.out.println("4. View Balance");
+        System.out.println("5. View Transaction History");
+        System.out.println("6. Change Password");
+        System.out.println("7. Logout");
+    }
+    public void displayMainMenu() {
+        System.out.println("1. Create Account");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
+    }
+    public void displayWelcomeMessage() {           
+        System.out.println("Welcome to the Bank Application!");
+    }   
+    
+}
